24c883c3bc16f91a83ba38246ac53539acd20f9c
Albert Kim <albertkim1234@gmail.com>
2021-04-25 20:14:39 -0500

    updated my files

--- albert.R
+++ albert.R
@@ -1,2 +1,16 @@
 library(tidyverse)
+library(cowplot)
+
+training_data_mlb <- read_csv('training_data_mlb.csv')
+
+# plot all variables by life expectancy, note any irregularities 
+training_data_mlb %>%
+  gather(-c(ops), key = "var", value = "value") %>%
+  ggplot(aes(x = value, y = ops)) + geom_point() + geom_smooth() + facet_wrap(~var, scales = "free")
+
+training_data_mlb %>%
+  gather(-c(ops), key = "var", value = "value") %>%
+  ggplot(aes(x = value)) + geom_histogram() + facet_wrap(~var, scales = "free")
+
+knitr::kable(cor(training_data_mlb %>% select(-c(ops))))
 
